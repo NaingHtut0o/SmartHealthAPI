8238a4a558dc1f3dfe3453d9cb59c5520d9be4e4
NaingHtut0o <nainghtutoo.life@gmail.com>
2025-03-06 15:59:38 +0630

    Test Controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHealthAPI.Utilities;
+
+namespace SmartHealthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<HealthCheckItemController> _logger;
+
+        public TestController(ILogger<HealthCheckItemController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Test")]
+        public async Task<IActionResult> Test()
+        {
+            return Ok("API running");
+        }
+    }
+}
